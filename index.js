5444772706ab85c42ca7ddfc24f6f4d28bc55265
tanvir <tanvir0046@gmail.com>
2024-01-10 23:14:13 +0600

    all work done

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3700
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.USER_ID}:${process.env.USER_KEY}@cluster0.huqehxg.mongodb.net/?retryWrites=true&w=majority`;
+const uri = "mongodb+srv://fabricfit241:FIshHNunqs5Ltgzq@cluster0.huqehxg.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -18,33 +18,39 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
+
+        const cardsCollection = client.db('fabricFit').collection("tShirts")
+
+
+        //card collections
+        app.get('/tShirts', async (req, res) => {
+            const result = await cardsCollection.find().toArray();
+            res.send(result)
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 app.get('/', (req, res) => {
